925b3b0ec45a9f424b820748d2996acfee5f73c9
ilyas <ilya_abramov_72@mail.ru>
2025-02-12 18:47:33 +0500

    logged errors

--- backend/internal/repository/task_repository.go
+++ backend/internal/repository/task_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 	"log/slog"
+	"os"
 	"todolist/backend/models"
 )
 
@@ -25,12 +26,17 @@ func (r *TaskRepository) InitDB() error {
 	status      TEXT NOT NULL CHECK(status IN ('Pending', 'In progress', 'Completed'))
 )`
 	_, err := r.db.Exec(query)
-	return err
+	if err != nil {
+		r.logger.Error("Failed to create table", "error", err)
+		os.Exit(1)
+	}
+	return nil
 }
 
 func (r *TaskRepository) GetTasks() ([]models.Task, error) {
 	rows, err := r.db.Query("SELECT * FROM tasks")
 	if err != nil {
+		r.logger.Error("Failed to execute SELECT query", "error", err)
 		return nil, err
 	}
 	defer rows.Close()
@@ -41,6 +47,7 @@ func (r *TaskRepository) GetTasks() ([]models.Task, error) {
 
 		err := rows.Scan(&task.ID, &task.Title, &task.Description, &task.Deadline, &task.Priority, &task.Status)
 		if err != nil {
+			r.logger.Error("Failed to scan rows", "error", err)
 			return nil, err
 		}
 
@@ -52,16 +59,25 @@ func (r *TaskRepository) GetTasks() ([]models.Task, error) {
 
 func (r *TaskRepository) InsertTask(title, description, deadline, priority string) error {
 	_, err := r.db.Exec("INSERT INTO tasks (title, description, deadline, priority) VALUES (?,?,?,?)", title, description, deadline, priority)
-	return err
+	if err != nil {
+		r.logger.Error("Failed to execute INSERT query", "error", err)
+		return err
+	}
+	return nil
 }
 
 func (r *TaskRepository) UpdateTask(id int, title, description, deadline, priority string) error {
 	_, err := r.db.Exec("UPDATE tasks SET title = ?, description = ?, deadline = ?, priority = ? WHERE id = ?", title, description, deadline, priority, id)
-	return err
+	if err != nil {
+		r.logger.Error("Failed to execute UPDATE query", "error", err)
+		return err
+	}
+	return nil
 }
 
 func (r *TaskRepository) DeleteTask(id int) error {
 	if _, err := r.db.Exec("DELETE FROM tasks WHERE id = ?", id); err != nil {
+		r.logger.Error("Failed to execute DELETE query", "error", err)
 		return err
 	}
 	return nil
