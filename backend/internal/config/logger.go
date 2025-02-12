d0ab4f9ccd5e1aae5a31e029c5108c240e6c8467
ilyas <ilya_abramov_72@mail.ru>
2025-02-12 18:11:39 +0500

    initialized logger

---
+++ backend/internal/config/logger.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"log/slog"
+	"os"
+	"path/filepath"
+)
+
+func NewLogger() *slog.Logger {
+	opts := &slog.HandlerOptions{
+		AddSource: true,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.SourceKey {
+				source, _ := a.Value.Any().(*slog.Source)
+				if source != nil {
+					source.File = filepath.Base(source.File)
+				}
+			}
+			return a
+		},
+	}
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, opts))
+
+	return logger
+}
