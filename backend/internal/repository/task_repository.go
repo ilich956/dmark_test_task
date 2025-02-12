f7adbe90a4799d5ea2de1fac5e76f17f0247e4d1
ilyas <ilya_abramov_72@mail.ru>
2025-02-12 18:10:47 +0500

    initialization of tasks table

--- backend/internal/repository/task_repository.go
+++ backend/internal/repository/task_repository.go
@@ -2,19 +2,34 @@ package repository
 
 import (
 	"database/sql"
+	"log/slog"
 	"todolist/backend/models"
 )
 
 type TaskRepository struct {
-	DB *sql.DB
+	db     *sql.DB
+	logger *slog.Logger
 }
 
-func NewTaskRepository(db *sql.DB) *TaskRepository {
-	return &TaskRepository{DB: db}
+func NewTaskRepository(db *sql.DB, logger *slog.Logger) *TaskRepository {
+	return &TaskRepository{db: db, logger: logger}
+}
+
+func (r *TaskRepository) InitDB() error {
+	query := `CREATE TABLE IF NOT EXISTS tasks(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+	deadline    DATETIME NOT NULL,
+	priority    TEXT NOT NULL CHECK(status IN ('Low', 'Medium', 'High'),
+	status      TEXT NOT NULL CHECK(status IN ('Pending', 'In progress', 'Completed'))
+)`
+	_, err := r.db.Exec(query)
+	return err
 }
 
 func (r *TaskRepository) GetTasks() ([]models.Task, error) {
-	rows, err := r.DB.Query("SELECT * FROM tasks")
+	rows, err := r.db.Query("SELECT * FROM tasks")
 	if err != nil {
 		return nil, err
 	}
@@ -36,17 +51,18 @@ func (r *TaskRepository) GetTasks() ([]models.Task, error) {
 }
 
 func (r *TaskRepository) InsertTask(title, description, deadline, priority string) error {
-	_, err := r.DB.Exec("INSERT INTO tasks (title, description, deadline, priority) VALUES (?,?,?,?)", title, description, deadline, priority)
+	_, err := r.db.Exec("INSERT INTO tasks (title, description, deadline, priority) VALUES (?,?,?,?)", title, description, deadline, priority)
 	return err
 }
 
 func (r *TaskRepository) UpdateTask(id int, title, description, deadline, priority string) error {
-	_, err := r.DB.Exec("UPDATE tasks SET title = ?, description = ?, deadline = ?, priority = ? WHERE id = ?", title, description, deadline, priority, id)
+	_, err := r.db.Exec("UPDATE tasks SET title = ?, description = ?, deadline = ?, priority = ? WHERE id = ?", title, description, deadline, priority, id)
 	return err
 }
 
 func (r *TaskRepository) DeleteTask(id int) error {
-	if _, err := r.DB.Exec("DELETE FROM tasks WHERE id = ?", id); err != nil {
+	if _, err := r.db.Exec("DELETE FROM tasks WHERE id = ?", id); err != nil {
 		return err
 	}
+	return nil
 }
