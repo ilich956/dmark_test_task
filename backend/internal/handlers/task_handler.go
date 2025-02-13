8df6f04efc8c3fa146589f6642fdb56d7c64f40d
ilyas <ilya_abramov_72@mail.ru>
2025-02-13 13:25:01 +0500

    put logger in handler

--- backend/internal/handlers/task_handler.go
+++ backend/internal/handlers/task_handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"log/slog"
 	myerrors "todolist/backend/internal/errors"
 	"todolist/backend/internal/services"
 	"todolist/backend/models"
@@ -8,32 +9,45 @@ import (
 
 type TaskHandler struct {
 	taskService *services.TaskService
+	logger      *slog.Logger
 }
 
-func NewTaskHandler(service *services.TaskService) *TaskHandler {
-	return &TaskHandler{taskService: service}
+func NewTaskHandler(service *services.TaskService, logger *slog.Logger) *TaskHandler {
+	return &TaskHandler{taskService: service, logger: &slog.Logger{}}
 }
 
 func (h *TaskHandler) GetTasksHandler() ([]models.Task, error) {
-	return h.taskService.GetTasksService()
+	tasks, err := h.taskService.GetTasksService()
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+	//fmt.Println(tasks)
 }
 
-func (h *TaskHandler) InsertTasksHandler(title, description, deadline, priority string) error {
+func (h *TaskHandler) InsertTasksHandler(title, description, deadline, priority, status string) error {
 	if title == "" {
+		h.logger.Error("Failed to insert task", "error", myerrors.ErrEmptyTitle)
 		return myerrors.ErrEmptyTitle
 	}
 
-	return h.taskService.InsertTaskService(title, description, deadline, priority)
+	//handle error
+
+	return h.taskService.InsertTaskService(title, description, deadline, priority, status)
 }
 
 func (h *TaskHandler) UpdateTasksHandler(id int, title, description, deadline, priority string) error {
 	if title == "" {
+		h.logger.Error("Failed to insert task", "error", myerrors.ErrEmptyTitle)
 		return myerrors.ErrEmptyTitle
 	}
 
+	//handle error
 	return h.taskService.UpdateTaskService(id, title, description, deadline, priority)
 }
 
 func (h *TaskHandler) DeleteTasksHandler(id int) error {
+	//handle error
+
 	return h.taskService.DeleteTaskService(id)
 }
